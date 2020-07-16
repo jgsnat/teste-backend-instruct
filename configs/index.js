cfffd13f0a6d230fbd333419e382b25552541657
Jefferson Silva <jgsnat@gmail.com>
2020-07-15 21:29:15 -0300

    - Adicionado CORs ao projeto;

--- configs/index.js
+++ configs/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = require('../routes');
+const allowCors = require('./cors');
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(allowCors);
 app.use(router);
 
 module.exports = app;
